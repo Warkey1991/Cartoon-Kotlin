457f1479fb280bd319282ff8023e269e0400db97
Warkey1991 <warkey1991@gmail.com>
2018-10-24 13:26:57 +0800

    first commit

---
+++ app/src/main/java/yj/song/cartoon/MainActivity.kt
@@ -0,0 +1,12 @@
+package yj.song.cartoon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
